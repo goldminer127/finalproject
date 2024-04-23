14e2a2ba7dcd8651292b2e6506a7910da9037a0e
smaggio123 <89543842+smaggio123@users.noreply.github.com>
2024-04-23 14:13:01 -0400

    Added pcp and attribute MDS implementations

--- client/src/Components/PCPcomponent/Pcp.js
+++ client/src/Components/PCPcomponent/Pcp.js
@@ -1,13 +1,356 @@
 import './Pcp.css';
 import * as d3 from 'd3';
+import { useEffect, useRef, useState } from "react";
+
+const {csv,select,scaleLinear,map,scaleBand,axisBottom,axisLeft,bin,max,range,format,selectAll,line,bisect} = d3;
+const margin = {top:20,bottom:20,right:10,left:40}
+const width = window.innerWidth * .66
+const height = window.innerHeight * .5
+//Holds thresholds attribute values have to be between. For categorical data, the threshold is the index of the category options
+var attributeTresholds = []
+//Holds coordinates for upper slider
+var slider1Coordinates = []
+//Holds coordinates for lower slider
+var slider2Coordinates = []
 
 function Pcp({colorMapping, categoryNames,allData,label, pcpAxisOrder, setPcpAxisOrder}) {
+    if(allData!=undefined&&allData[0]!=undefined){
+        let listOfLists = []
+        
+        for(let i = 0;i<allData.length;i++){
+            let tempDataList = []
+            for(let k = 0;k<allData[0].length;k++){
+                if(k!=1){
+                    tempDataList.push(allData[i][k])
+                }
+                else{
+                    let arr = allData[i][k].split("-")
+                    tempDataList.push(arr[0])
+                    tempDataList.push(arr[1])
+                }
+            }
+            listOfLists.push(tempDataList)
+        }
+        allData = listOfLists
+        let tempCategoryList = []
+        for(let i = 0;i<categoryNames.length;i++){
+            if(i!=1){
+                tempCategoryList.push(categoryNames[i])
+            }
+            else{
+                tempCategoryList.push("Year")
+                tempCategoryList.push("Month")
+            }
+        }
+        categoryNames=tempCategoryList
+
+    }
     console.log(allData)
     console.log(label)
-
+    console.log(categoryNames)
+    const svgRef = useRef();
+    useEffect(() => {
+        if(allData!=undefined&&allData[0]!=undefined){
+        const svg = d3.select(svgRef.current)
+            .attr('width', width)
+            .attr('height', height)
+            .style('overflow', 'visible');
+        svg.selectAll('*').remove();
+        let axisIndex = 0;
+    
+        const pcpXDomain = [0,allData[0].length];
+        const pcpXRange = [margin.left,width - margin.right]
+        const pcpXScale = scaleLinear(pcpXDomain,pcpXRange)
+        //Making the svg where the chart will go
+        // const PCP = select('#pcpContainer').append('svg').attr('width',width).attr('height',height)
+        svg.append("text").attr('id','svgplot').style("text-anchor", "middle").attr("x", width/2).attr("y", 20).text('Parallel Coordinate Plot').style("fill", "white")
+        
+        let dictOfScales = {}
+        //Making axes
+        for(let j = 0;j<allData[0].length;j++){
+            if(typeof allData[0][j] =="string"){
+                const OPTIONS = Array.from(new Set(allData.map(row => row[j])));
+                //The domain is set to the options
+                const yDomain = OPTIONS
+                const yRange = [height - margin.bottom, margin.top+margin.top]
+                const yScale = scaleBand(yDomain,yRange).padding(1)
+                dictOfScales[categoryNames[j]] = yScale
+                attributeTresholds.push([0,OPTIONS.length-1])
+                const yAxis= axisLeft(yScale)
+                svg.append("g")    
+                .attr("transform", `translate(${pcpXScale(j)}, 0)`)
+                .attr('id', 'pcpAxis' + axisIndex)
+                .attr('class', 'pcpAxis')
+                .style('stroke-width', '2px')
+                .call(d3.drag()
+                .on('drag', function(event) {
+                    select(this).attr("transform", `translate(${Math.max(margin.left, Math.min(width - margin.right, event.x))}, 0)`);
+                })
+                .on('end',function(event){
+                    //When users drops the slider, it snaps to the nearest tick
+                    var listOfxScaleValues = []
+                    for(let index = 0;index<categoryNames.length;index++){
+                        listOfxScaleValues.push(pcpXScale(index))
+                    }
+                    // Snap to the nearest category position
+                    var xPos = event.x
+        
+                    var closestCategoryIndex = 0
+                    for(let index = 0;index<=listOfxScaleValues.length;index++){
+                        if(index == listOfxScaleValues.length-1){
+                            if(xPos<=listOfxScaleValues[index]){
+                                closestCategoryIndex = index
+                                break
+                            }
+                        }
+                        else{
+                            if(xPos>=listOfxScaleValues[index]&&xPos<=listOfxScaleValues[index+1]){
+                                let midpoint = (listOfxScaleValues[index]+listOfxScaleValues[index+1])/2
+                                if(xPos<midpoint){
+                                    closestCategoryIndex = index
+                                }
+                                else{
+                                    closestCategoryIndex = index+1
+                                }
+                                break
+                            }
+                        }
+                    }
+                /*
+                var temp = pcpIndicies[j];
+                pcpIndicies[j] = pcpIndicies[closestCategoryIndex]
+                pcpIndicies[closestCategoryIndex] = temp;
+                makePcpPlot()
+                */
+                })
+            ).call(yAxis).append("text").style("text-anchor", "middle").attr('id','pcpText'+axisIndex).attr("y", margin.top*1.5).text(categoryNames[j]).style("fill", "white")
+            /*
+            //Upper slider
+            svg.append("circle")
+                .attr("cx", pcpXScale(j))
+                .attr("cy", slider1Coordinates[j])
+                .attr("r", 5)
+                .style("fill", "blue")
+                .call(d3.drag()
+                .on("drag", function(event) {
+                    select(this).attr("cy", event.y)
+                })
+                .on("end", function() {
+                    //When users drops the slider, it snaps to the nearest tick
+                    var listOfyScaleValues = []
+                    for(let index = 0;index<yScale.domain().length;index++){
+                        listOfyScaleValues.push(yScale(yScale.domain()[index]))
+                    }
+                    // Snap to the nearest category position
+                    var yPos = select(this).attr("cy")
+                    
+                    var closestCategoryIndex = 0
+                    for(let index = 0;index<=listOfyScaleValues.length;index++){
+                        if(index == listOfyScaleValues.length-1){
+                            if(yPos<=listOfyScaleValues[index]){
+                                closestCategoryIndex = index
+                                break
+                            }
+                        }
+                        else{
+                            if(yPos<=listOfyScaleValues[index]&&yPos>=listOfyScaleValues[index+1]){
+                                let midpoint = (listOfyScaleValues[index]+listOfyScaleValues[index+1])/2
+                                if(yPos>midpoint){
+                                    closestCategoryIndex = index
+                                }
+                                else{
+                                    closestCategoryIndex = index+1
+                                }
+                                break
+                            }
+                        }
+                    }
+                    var yValue = listOfyScaleValues[closestCategoryIndex];
+                    // Move the slider to the snapped position
+                    select(this).attr("cy", yValue); 
+                    attributeTresholds[pcpIndicies[j]][1] = closestCategoryIndex
+                    slider1Coordinates[pcpIndicies[j]] = yValue
+                    makePcpPlot()
+                }))
+            //Lower slider
+            svg.append("circle")
+            .attr("cx", pcpXScale(j))
+            .attr("cy", slider2Coordinates[pcpIndicies[j]])
+            .attr("r", 5)
+            .style("fill", "red")
+            .call(d3.drag()
+            .on("drag", function(event) {
+                select(this).attr("cy", event.y)
+            })
+            .on("end", function() {
+                var listOfyScaleValues = []
+                for(let index = 0;index<yScale.domain().length;index++){
+                    listOfyScaleValues.push(yScale(yScale.domain()[index]))
+                }
+                // Snap to the nearest category position
+                var yPos = select(this).attr("cy")
+                
+                var closestCategoryIndex = 0
+                for(let index = 0;index<=listOfyScaleValues.length;index++){
+                    if(index == listOfyScaleValues.length-1){
+                        if(yPos<=listOfyScaleValues[index]){
+                            closestCategoryIndex = index
+                            break
+                        }
+                    }
+                    else{
+                        if(yPos<=listOfyScaleValues[index]&&yPos>=listOfyScaleValues[index+1]){
+                            let midpoint = (listOfyScaleValues[index]+listOfyScaleValues[index+1])/2
+                            if(yPos>midpoint){
+                                closestCategoryIndex = index
+                            }
+                            else{
+                                closestCategoryIndex = index+1
+                            }
+                            break
+                        }
+                    }
+                }
+                var yValue = listOfyScaleValues[closestCategoryIndex];
+                // Move the slider to the snapped position
+                select(this).attr("cy", yValue); 
+                attributeTresholds[pcpIndicies[j]][0] = closestCategoryIndex
+                slider2Coordinates[pcpIndicies[j]] = yValue
+                makePcpPlot()
+            }))
+            */
+            axisIndex++
+            }
+            else{
+                const dataColumnj = allData.map(row => row[j]);
+                const yDomain = [Math.min(...dataColumnj)-(Math.min(...dataColumnj)*.01),Math.max(...dataColumnj)+(Math.max(...dataColumnj)*.01)]
+                const yRange = [height - margin.bottom, margin.top+margin.top]
+                const yScale = scaleLinear(yDomain,yRange)
+                const invertedYscale = scaleLinear(yRange,yDomain)
+                attributeTresholds.push([invertedYscale.range()[0],invertedYscale.range()[1]])
+                dictOfScales[categoryNames[j]] = yScale
+                const yAxis= axisLeft(yScale)
+                yAxis.tickFormat(d3.format(".0e"));
+                svg.append("g")
+                .attr("transform", `translate(${pcpXScale(j)}, 0)`)
+                .attr('id', 'pcpAxis' + axisIndex)
+                .attr('class', 'pcpAxis')
+                .style('stroke-width', '2px')
+                .call(d3.drag()
+                .on('drag', function(event) {
+                    select(this).attr("transform", `translate(${Math.max(margin.left, Math.min(width - margin.right, event.x))}, 0)`);
+                })
+               .on('end',function(event){
+                    //When users drops the slider, it snaps to the nearest tick
+                    var listOfxScaleValues = []
+                    for(let index = 0;index<allData[0].length;index++){
+                        listOfxScaleValues.push(pcpXScale(index))
+                    }
+                    // Snap to the nearest category position
+                    var xPos = event.x
+        
+                    var closestCategoryIndex = 0
+                    for(let index = 0;index<=listOfxScaleValues.length;index++){
+                        if(index == listOfxScaleValues.length-1){
+                            if(xPos<=listOfxScaleValues[index]){
+                                closestCategoryIndex = index
+                                break
+                            }
+                        }
+                        else{
+                            if(xPos>=listOfxScaleValues[index]&&xPos<=listOfxScaleValues[index+1]){
+                                let midpoint = (listOfxScaleValues[index]+listOfxScaleValues[index+1])/2
+                                if(xPos<midpoint){
+                                    closestCategoryIndex = index
+                                }
+                                else{
+                                    closestCategoryIndex = index+1
+                                }
+                                break
+                            }
+                        }
+                    }
+                    /*
+                    var temp = pcpIndicies[j];
+                    pcpIndicies[j] = pcpIndicies[closestCategoryIndex]
+                    pcpIndicies[closestCategoryIndex] = temp;
+                    makePcpPlot()
+                    */
+                })
+            ).call(yAxis).append("text").style("text-anchor", "middle").attr('id','pcpText'+axisIndex).attr("y", margin.top*1.5).text(categoryNames[j]).style("fill", "white")
+            /*
+            //Upper slider
+            svg.append("circle")
+                .attr("cx", pcpXScale(j))
+                .attr("cy", slider1Coordinates[pcpIndicies[j]])
+                .attr("r", 5)
+                .style("fill", "blue")
+                .call(d3.drag()
+                .on("drag", function(event) {
+                    select(this).attr("cy", Math.max(margin.top*2,Math.min(height-margin.bottom,event.y)))
+                })
+                .on("end", function() {
+                    var yPos = select(this).attr("cy")
+                    attributeTresholds[pcpIndicies[j]][1] = invertedYscale(yPos)
+                    slider1Coordinates[pcpIndicies[j]] = yPos
+                    makePcpPlot()
+                }))
+            //Lower slider
+            svg.append("circle")
+                .attr("cx", pcpXScale(j))
+                .attr("cy", slider2Coordinates[pcpIndicies[j]])
+                .attr("r", 5)
+                .style("fill", "red")
+                .call(d3.drag()
+                .on("drag", function(event) {
+                    select(this).attr("cy", Math.max(margin.top*2,Math.min(height-margin.bottom,event.y)))
+                })
+                .on("end", function() {
+                    var yPos = select(this).attr("cy")
+                    attributeTresholds[pcpIndicies[j]][0] = invertedYscale(yPos)
+                    slider2Coordinates[pcpIndicies[j]] = yPos
+                    makePcpPlot()
+                }))
+            */
+            axisIndex++
+            }
+            
+        }
+        
+       //Making the lines that go through the axes
+       for(let j = 0;j<allData.length;j++){
+           let validRow = true;
+           for(let index = 0;index<categoryNames.length;index++){
+               if(typeof allData[j][index] === 'string'){
+                   let valueIndex = dictOfScales[categoryNames[index]].domain().indexOf(allData[j][index])
+                   if(valueIndex<attributeTresholds[index][0] || valueIndex>attributeTresholds[index][1]){
+                       index = categoryNames.length+1
+                       validRow=false
+                    }
+                }
+                else{
+                    let valueOfRowElement = parseFloat(allData[j][index])
+                    if(valueOfRowElement<attributeTresholds[index][0] || valueOfRowElement>attributeTresholds[index][1]){
+                        index = categoryNames.length+1
+                        validRow=false
+                    }
+                }
+            }
+            if(validRow){
+                for(let index = 1;index<categoryNames.length;index++){
+                    svg.append("path").attr("id", "line"+label[j]).style("fill", "none" ).style("stroke", colorMapping[label[j]]).style("opacity", '0.8').attr("d",(d,i)=>{
+                        return 'M'+pcpXScale(index-1)+","+(dictOfScales[categoryNames[index-1]](allData[j][index-1]))+ ' L' + pcpXScale(index)+","+(dictOfScales[categoryNames[index]](allData[j][index]))
+                    })
+                }
+            }
+            
+        }
+        
+    }
+    })
     return(
         <>
-            <div>Big Div</div>
+            <svg ref={svgRef}></svg>
         </>
     )
 }
